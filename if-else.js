9682e4d20ee7f365c03a4bc97cab484b18cac7f6
unknown <you@example.com>
2024-08-07 01:19:11 +0600

    hahah

---
+++ if-else.js
@@ -0,0 +1,20 @@
+var money = 500;
+var phnAse = true;
+
+
+// if only.
+// judi tk besi thake khabo
+
+// if(money > 300){
+//     console.log('taile kisu khabo')
+// }
+
+// na thakle ki??
+
+// else
+if(money < 300){
+    console.log('khabo')
+}
+else {
+    console.log('phn tipbo')
+}
---
+++ multiple.js
@@ -0,0 +1,59 @@
+var money = 4000
+var isBsc = true
+var hasCar = false
+
+// both condition have to need 
+//  if(money > 3000 & hasCar == true){
+//      console.log('taile ber hobo')
+
+//  }
+// else{
+//     console.log('hobo na')
+// }
+
+// all needs are fullfilled
+
+// if(money > 3000 && isBsc == true ){
+//     console.log('taile biya hobe')
+// }
+// else{
+//     console.log('hobe na')
+
+// }
+
+
+// its otherwise that
+
+// if(money > 3000 || hasCar == true ){
+//     console.log('taile biya hobe')
+// }
+// else{
+//     console.log('hobe na')
+// }
+
+// all wants are not fullfilled
+// if(money > 6000 || hasCar == true ){
+//     console.log('taile biya hobe')
+// }
+// else{
+//     console.log('hobe na')
+// }
+
+//  ....................complex  condition....................
+
+// if((money > 6000 && hasCar == true ) || isBsc == true ){
+//     console.log('taile biya hobe')
+// }
+// else{
+//     console.log('hobe na')
+// }      
+
+
+
+
+if((money > 3000 || hasCar == true ) & isBsc == true ){
+    console.log('taile biya hobe')
+}
+else{
+    console.log('hobe na')
+}
